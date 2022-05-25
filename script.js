701dca80375dca0e60f81023ad803d54856f7697
Hans Reichgelt <hreichgelt06@gmail.com>
2022-05-24 22:25:39 -0400

    additional steps written out in script and moved to readme

---
+++ script.js
@@ -0,0 +1,23 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// 1. build a button in html 
+// 2. Create a countdown
+// 3. create prompts 
+// 4. create if statements 
+// 5. create for lopps 
+// 6. style the page with css
+
+// Where everything is - STOP AT LESSON 14
+// timer is in UNC... 09 Timers
+// event listeners - click buttuns are in 11
+// we only go up to lesson 14 so far
